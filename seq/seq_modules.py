b6ce8b0c1ef5c3d1957c871da8d5c8b84c219749
Rafael Corsi <corsiferrao@gmail.com>
2022-07-27 16:30:32 -0300

    seq agora já usa intbv

--- seq/seq_modules.py
+++ seq/seq_modules.py
@@ -20,35 +20,6 @@ def dff(q, d, clk, rst):
 
 
 @block
-def blinkLedAdder(led, clk, rst):
-    x = [Signal(bool(0)) for i in range(32)]
-    y = [Signal(bool(0)) for i in range(32)]
-    s = [Signal(bool(0)) for i in range(32)]
-    c = Signal(bool(0))
-    status = Signal(bool(0))
-
-    y[0] = 1
-    adder_1 = adder(x, y, s, c)
-
-    @always_seq(clk.posedge, reset=rst)
-    def seq():
-        if x[24] == 0 and x[23] == 0:
-            for i in range(len(x)):
-                x[i].next = s[i]
-            status.next = status
-        else:
-            for i in range(len(x)):
-                x[i].next = 0
-            status.next = not status
-
-    @always_comb
-    def comb():
-        led.next = status
-
-    return instances()
-
-
-@block
 def blinkLed(led, time_ms, clk, rst):
     ms = 50000
     cnt = Signal(intbv(0)[32:])
