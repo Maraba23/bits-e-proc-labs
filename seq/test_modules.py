fe6b9fa4b0951a20276293f305085498f6396bc6
Rafael Corsi <corsiferrao@gmail.com>
2022-07-27 13:11:49 -0300

    tentando implementar o bin2bcd

--- seq/test_modules.py
+++ seq/test_modules.py
@@ -1,25 +1,28 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 from myhdl import *
-from ula_modules import *
+from seq_modules import *
 
 
-def test_dff():
+@block
+def test_dd():
+    rst = ResetSignal(0, active=1, isasync=True)
+    clk = Signal(bool(0))
+    din = Signal(intbv(14)[8:])
+    d_1 = doubleDabble(din, clk, rst)
+
+    @always(delay(1))
+    def clkgen():
+        clk.next = not clk
+
     @instance
     def stimulus():
-        for i in range(4):
-            t = bin(i, 2)
-            a.next = bool(int(t[0]))
-            b.next = bool(int(t[1]))
-            yield delay(1)
-            # print("{} {} {} {}".format(a, b, soma, vaiUm))
-            assert soma == (a ^ b)
-            assert vaiUm == (a and b)
+        yield delay(1)
 
-    a = Signal(bool(0))
-    b = Signal(bool(0))
-    soma = Signal(bool(0))
-    vaiUm = Signal(bool(0))
-    dut = halfAdder(a, b, soma, vaiUm)
-    sim = Simulation(dut, stimulus)
-    sim.run()
+    return instances()
+
+
+tb = test_dd()
+# sim = Simulation(tb)
+tb.run_sim(200)
